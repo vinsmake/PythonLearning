a9f4aa52a73b6951080ed09fc3ceb2e6b1e1e905
vinsmake <ps.enrique.e@gmail.com>
2023-05-20 08:06:34 -0600

    chore |Updated.

---
+++ Day1 - print-text.py
@@ -0,0 +1,17 @@
+print('Me llamo \'Enrique\' \nEsta es mi primer string de python')
+print('\tEsta es una linea tabulada')
+print('Esta es una barra invertida \\')
+
+print('Línea 1 \nLínea 2 \nLínea 3')
+
+print('A\tB\tC\nD\tE\tF\nG\tH\tI')
+print('Barra Normal: /\nBarra Invertida: \\')
+
+print('Tu nombre es ' + input('Dime tu nombre: ') + ' ' + input('Dime tu apellido: '))
+
+
+print(input('¿Qué estás estudiando?'))
+print(input('¿En qué país vives?'))
+
+print(input('Escribe tu nombre:') + ' ' + input('Escribe tu apellido:'))
+
